8c4a7b0bb7946263bb1bd3361a29e11579cc9ea2
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2025-11-24 00:22:41 +0100

    debugger update

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,12 +1,12 @@
 /** biome-ignore-all lint/suspicious/noExplicitAny: it's a logger */
-const IS_DEV = import.meta.env.DEV;
+const IS_DEBUG = import.meta.env.DEBUG;
 
 export const logger = {
 	debug(...args: any[]) {
-		if (IS_DEV) console.log("[pumble-colors]", ...args);
+		if (IS_DEBUG) console.log("[pumble-colors]", ...args);
 	},
 	info(...args: any[]) {
-		console.info("[pumble-colors]", ...args);
+		if (IS_DEBUG) console.info("[pumble-colors]", ...args);
 	},
 	warn(...args: any[]) {
 		console.warn("[pumble-colors]", ...args);
