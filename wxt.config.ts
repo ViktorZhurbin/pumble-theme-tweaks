fb5e89ee5ba76ff897c2675142cdde8c086bb984
Viktor Zhurbin <vzhurbin@gmail.com>
2025-12-03 12:50:30 +0100

    bump version

--- wxt.config.ts
+++ wxt.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
 	modules: ["@wxt-dev/module-solid"],
 	manifest: {
 		name: "Pumble Tweaks",
-		version: "1.1.1",
+		version: "1.1.2",
 		description: "Change Pumble colors in web browser.",
 		permissions: ["storage", "scripting"],
 		host_permissions: [...PUMBLE_URL_PATTERNS],
