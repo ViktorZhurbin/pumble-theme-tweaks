f8151f59185a42f2f010f7cfc8c618e10332c8db
Viktor Zhurbin <vzhurbin@gmail.com>
2025-12-10 12:36:04 +0100

    works for beta as well

--- src/constants/pumble-urls.ts
+++ src/constants/pumble-urls.ts
@@ -1,6 +1,6 @@
 export const PUMBLE_URL = {
 	PROD: "app.pumble.com",
-	STAGE: "app.stage.ops.pumble.com",
+	STAGE: "stage.ops.pumble.com",
 	DEV: "fe.pumble-dev.com",
 };
 
@@ -9,6 +9,6 @@ export const PUMBLE_URL = {
  */
 export const PUMBLE_URL_PATTERNS = [
 	`https://${PUMBLE_URL.PROD}/*`,
-	`https://${PUMBLE_URL.STAGE}/*`,
+	`https://*.${PUMBLE_URL.STAGE}/*`, // Matches BETA as well
 	`https://*.${PUMBLE_URL.DEV}/*`,
 ];
--- src/lib/url.ts
+++ src/lib/url.ts
@@ -4,10 +4,6 @@ export const UrlUtils = {
 	isPumbleUrl(url?: string): boolean {
 		if (!url) return false;
 
-		return (
-			url.includes(PUMBLE_URL.PROD) ||
-			url.includes(PUMBLE_URL.STAGE) ||
-			url.includes(PUMBLE_URL.DEV)
-		);
+		return Object.values(PUMBLE_URL).some((urlPart) => url.includes(urlPart));
 	},
 };
